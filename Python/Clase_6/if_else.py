324304ea748c382063eccc9bf7e09799f2028f70
Betsabé Gutierrez Santos <daianagutierrez2004@gmail.com>
2024-06-04 23:54:11 -0300

    aprendiendo nuevos comandos en GIT

--- Python/Clase_6/if_else.py
+++
@@ -1,8 +0,0 @@
-# En esta calse veremos al sentencia IF/ELSE
-condicion = 10
-if condicion == True:
-    print("Condicion VERDADERA")
-elif condicion == False:
-    print("Condicion FALSA")
-else:
-    print("Condicion sin especificar")
