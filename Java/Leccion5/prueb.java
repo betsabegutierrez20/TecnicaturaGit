b44816e27b2335452b553f69870cec1c5a30e344
Betsabé Gutierrez Santos <daianagutierrez2004@gmail.com>
2024-06-22 23:25:50 -0300

    Actualizacion de Java ejercicios

---
+++ Java/Leccion5/prueb.java
@@ -0,0 +1,10 @@
+package Java.Leccion5;
+
+public class prueb {
+    public static void main(String[] args){
+        var nombre= "Ariel";
+        System.out.println(nombre);
+        nombre = "Pablo";
+        System.out.println(nombre);
+    }
+}
